b9f59362d8738efd8e253b1e2f17adb47844ff96
Tasfiq Asif <ggunnerr@gmail.com>
2020-07-31 19:15:01 +1000

    fixed

--- unit-test.py
+++ unit-test.py
@@ -9,10 +9,10 @@ class TestSum(unittest.TestCase):
         self.assertEqual(add(10, 90), 100)
 
     def test_substract_operation_returns_correct_value(self):
-        self.assertEqual(subtract(100, 90), 100)
+        self.assertEqual(subtract(100, 90), 10)
 
     def test_multiply_operation_returns_incorrect_value(self):
-        self.assertEqual(multiply(10, 10), 1000)
+        self.assertEqual(multiply(10, 10), 100)
 
     def test_division_operation_returns_incorrect_value(self):
         self.assertEqual(divide(100, 10), 10)
