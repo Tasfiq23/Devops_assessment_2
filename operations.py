f157ae94b0831f54905397143d391a67d2a2bffb
Tasfiq Asif <ggunnerr@gmail.com>
2020-07-31 11:35:33 +1000

    change in divide error message

--- operations.py
+++ operations.py
@@ -14,11 +14,12 @@ def divide(a, b):
     try:
         return a / b
     except ZeroDivisionError:
-        return "Zero Division Error!!!"
+        return "Zero Division Error!"
 
 
 def power(a, b):
     return "Power operation is not supported."
+
     # try:
     #     return a / b
     # except ZeroDivisionError:
